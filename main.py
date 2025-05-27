58e58001a844635e1aac2df8c300e55f74cb66b8
Ashley <panelo.ashley@gmail.com>
2025-05-27 23:38:37 +0800

    Added Funtionalies to 'Add' button

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import tkinter
+from tkinter import messagebox
+
+DEFAULT_EMAIL = "sample@gmail.com"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_data():
+    website = web_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    is_confirmed = messagebox.askokcancel(title=website, message=f"Email: {email}\nPassword: {password}\n\nAdd these login info?")
+
+    if is_confirmed:
+        with open("web_data.txt", "a") as data:
+            data.write(f"{website} | {email} | {password}\n")
+            web_entry.delete(0, tkinter.END)
+            password_entry.delete(0, tkinter.END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 # Canvas
 lock_image_canvas = tkinter.Canvas(width=200, height=200)
@@ -17,7 +33,7 @@ lock_image_canvas.grid(column=1, row=0)
 
 # Labels
 web_label = tkinter.Label(text="Website:")
-web_label.grid(column=0,row=1,)
+web_label.grid(column=0,row=1)
 
 email_label = tkinter.Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
@@ -26,19 +42,21 @@ password_label = tkinter.Label(text="Password:")
 password_label.grid(column=0, row=3)
 
 # Entry
-web_entry = tkinter.Entry(width=45)
-web_entry.grid(column=1, row=1, columnspan=2, sticky="w")
+web_entry = tkinter.Entry()
+web_entry.focus()
+web_entry.grid(column=1, row=1, columnspan=2, sticky="ew")
 
-email_entry = tkinter.Entry(width=45)
-email_entry.grid(column=1,row=2, columnspan=2, sticky="w")
+email_entry = tkinter.Entry()
+email_entry.insert(0,DEFAULT_EMAIL)
+email_entry.grid(column=1,row=2, columnspan=2, sticky="ew")
 
-password_entry = tkinter.Entry(width=28)
-password_entry.grid(column=1, row=3, sticky="w")
+password_entry = tkinter.Entry()
+password_entry.grid(column=1, row=3, sticky="ew")
 
 # Buttons
 generate_pass = tkinter.Button(text="Generate Password")
-generate_pass.grid(column=2, row=3, sticky="w")
+generate_pass.grid(column=2, row=3, sticky="ew")
 
-add_button = tkinter.Button(text="Add", width=38)
-add_button.grid(column=1, row=4, columnspan=2, sticky="w")
+add_button = tkinter.Button(text="Add", command=save_data)
+add_button.grid(column=1, row=4, columnspan=3, sticky="ew")
 window.mainloop()
